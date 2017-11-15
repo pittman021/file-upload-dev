3b1f8637ccef40765c1bf2329684f867ab3ce94d
Tim Pittman <tim.pittman021@gmail.com>
2017-11-14 23:47:42 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
+const app = express();
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize('file_upload_database', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+// testing DB Connection //
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('connection established');
+  })
+  .catch(err => {
+    console.err('unable to connect', err);
+  });
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+const File = sequelize.define('file', {
+  filename: Sequelize.STRING,
+  path: Sequelize.STRING
+});
+
+// Creating User Table
+User.sync({ force: true }).then(() => {
+  return User.create({
+    firstName: 'Tim',
+    lastName: 'Pittman'
+  });
+});
+
+File.sync({ force: true }).then(() => {
+  return File.create({
+    filename: 'myfile.js',
+    path: '/users/files/path'
+  });
+});
+
+File.belongsTo(User);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log('server running on', PORT);
+});
